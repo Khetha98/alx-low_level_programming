4f8e8b73654ed78222f074b7e3df995adfe6e67f
Khetha98 <Khethukuthulasimamane@gmail.com>
2023-02-19 03:42:23 -0800

    This is what I commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - the entry point
+ *
+ * Return - 0 (indicate success)
+ * */
+
+int main(void)
+{
+ printf("Programming is like building a multilingual puzzle");
+ return (0);
+
+
+}
