5575f10b2fc9aa1bc04c7ad4235fa80069ddc6b5
Khetha98 <Khethukuthulasimamane@gmail.com>
2023-02-20 10:03:08 -0800

    My commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+char c = 'a';
+char n = '0';
+while (n <= '9')
+{
+putchar(n);
+n++;
+}
+while (c <= 'f')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return 0;
+}
