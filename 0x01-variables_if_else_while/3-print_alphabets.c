4b495bae7bb6d57319dfa4bf1403315955839130
Khetha98 <Khethukuthulasimamane@gmail.com>
2023-02-20 02:20:49 -0800

    One of my commits of many

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * */
+int main(void)
+{
+char ch = 'a';
+char cha = 'a';
+while (ch <='z')
+{
+putchar(ch);
+ch++;
+}
+
+while (cha <='z')
+{
+putchar(toupper(cha));
+cha++;
+}
+
+
+return (0);
+}
