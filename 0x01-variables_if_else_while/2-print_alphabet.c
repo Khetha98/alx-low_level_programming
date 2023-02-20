4f33e7ffc4c93539393543b70f22edcbfa674194
Khetha98 <Khethukuthulasimamane@gmail.com>
2023-02-20 00:19:28 -0800

    Removing white spaces

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,13 @@
  **/
 
 int main(void)
-{  
-char ch = 'a';  
-while (ch <= 'z')  
-{  
-putchar(ch);  
-ch++;  
-}  
-putchar('\n');  
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
 return (0);
 }
