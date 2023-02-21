e8b5dae3d816dfa76f592c14ebfc93e8ac90ddd7
Khetha98 <Khethukuthulasimamane@gmail.com>
2023-02-21 15:34:16 -0800

    commit final

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,6 +4,7 @@
 
 /**
  * _islower - see description below
+ * @c: it is a parameter passed to a function
  * Description: checks if a letter is lowercase
  * Return: gives 1 if it letter is lowercase else 0
  **/
