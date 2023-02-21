ef08490c65076b3dc4286a2b07debe287b21f558
Khetha98 <Khethukuthulasimamane@gmail.com>
2023-02-21 10:30:23 -0800

    another version of 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,24 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
  *
- * Return: Always 0 (Success)
+ * Return: Always void
  **/
-
-int main(void)
-{
-print_alphabet();
-return (0);
-}
 
 void print_alphabet(void)
 {
 char c = 'a';
 while (c <= 'z')
 {
-putchar(c);
+_putchar(c);
 c++;
 }
-putchar('\n');
+_putchar('\n');
 }
