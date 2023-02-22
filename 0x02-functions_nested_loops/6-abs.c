ab73c681ab0d51689a2119094c9afdf0ff9c7ccc
Khetha98 <Khethukuthulasimamane@gmail.com>
2023-02-21 17:00:22 -0800

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_abs - Check out description
+ * @n: Is an input parameter
+ * Description: Give out the absolute value of a number
+ * Return: Absolut value of number n
+ */
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (n * -1);
+}
