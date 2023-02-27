f0727cbeda6442ce7f4d492c8a3fa999779793b7
Khetha98 <Khethukuthulasimamane@gmail.com>
2023-02-27 09:43:21 -0800

    lenght of character function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * int _strlen - this function takes in a character and
+ * give out its lenght
+ * @s: input parameter of char type
+ * Return: integer
+ **/
+
+int _strlen(char *s);
+{
+
+return strlen(s);
+}
