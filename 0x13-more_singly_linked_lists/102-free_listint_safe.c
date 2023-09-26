e7c68d534db86f3a3be875398888bbc9bc995df9
Khetha98 <Khethukuthulasimamane@gmail.com>
2023-09-26 02:09:04 +0200

    last commit

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 
 /**
- * _ra - reallocates memory for an array of pointers
- * to the nodes in a linked list
- * @list: the old list to append
- * @size: size of the new list (always one more than the old list)
- * @new: new node to add to the list
+ * _ra - reallocates memory for the array of pointers
+ * to the nodes in the linked list
+ * @list: the old list to append to
+ * @size: size of a new list (always one more than the old list)
+ * @new: new node to add to a list
  *
- * Return: pointer to the new list
+ * Return: pointer to a new list
  */
 listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 {
@@ -30,11 +30,12 @@ listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 }
 
 /**
- * free_listint_safe - frees a listint_t linked list.
- * @head: double pointer to the start of the list
+ * free_listint_safe - frees the listint_t linked list.
+ * @head: double pointer to start of a list
  *
- * Return: the number of nodes in the list
+ * Return: the number of the nodes in the list
  */
+
 size_t free_listint_safe(listint_t **head)
 {
 	size_t i, num = 0;
