0f22154dfba259db2dc50867eac95d7908c5eb10
Khetha98 <Khethukuthulasimamane@gmail.com>
2023-09-28 21:57:43 +0200

    file change

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -19,12 +19,10 @@ void print_binary(unsigned long int n)
 		if (current & 1)
 		{
 			_putchar('1');
-			count++
-
+			count++;
 		}
 		else if (count)
 			_putchar('0');
-
 	}
 	if (!count)
 		_putchar('0');
