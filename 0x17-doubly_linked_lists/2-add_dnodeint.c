cb30f131c331a65f35349f5256d6fd287fc16679
Khetha98 <Khethukuthulasimamane@gmail.com>
2023-11-09 22:15:02 +0200

    task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -32,11 +32,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		*head = node;
 		return (*head);
 	}
-    
+
 	temp = *head;
 	node->next = temp;
 	temp->prev = node;
 	*head = node;
 	return (node);
 }
-
