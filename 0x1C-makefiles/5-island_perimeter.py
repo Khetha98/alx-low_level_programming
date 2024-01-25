35cbbccc5133b70d4fa5bd6225b9cec9a465ec4a
Khetha98 <Khethukuthulasimamane@gmail.com>
2024-01-25 07:25:17 +0200

    files

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""It defines a function islnd_perimeter"""
+
+
+def island_perimeter(grid):
+    """Gives out the perimeter of an island"""
+
+    w = len(grid[0])
+    h = len(grid)
+    e = 0
+    s = 0
+
+    for i in range(h):
+        for j in range(w):
+            if grid[i][j] == 1:
+                s += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    e += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    e += 1
+    return s * 4 - e * 2
