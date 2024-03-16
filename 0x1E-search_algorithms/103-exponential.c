c0e6f7e9fd2e75f423baab9c1dee101516307e0b
Khetha98 <Khethukuthulasimamane@gmail.com>
2024-03-16 18:07:58 +0200

    task 103

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,63 +1,39 @@
 #include "search_algos.h"
 
 /**
- * searching - searches for the value in an array of
- * integers using the Binary search algorithm
- *
- *
- * @array: is the input array
- * @size: it the size of the array
- * @value: is a value to search in
- * Return: index of the number
- */
-int searching(int *array, size_t size, int value)
-{
-	size_t half = size / 2;
-	size_t i;
-
-	if (array == NULL || size == 0)
-		return (-1);
-
-	printf("Searching in array");
-
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-
-	printf("\n");
-
-	if (half && size % 2 == 0)
-		half--;
-
-	if (value == array[half])
-		return ((int)half);
-
-	if (value < array[half])
-		return (searching(array, half, value));
-
-	half++;
-
-	return (searching(array + half, size - half, value) + half);
-}
-
-/**
- * bn_search - calls to bn_search function to return
+ * binary_search - calls to bn_search function to return
  * the index of the number
  *
  * @array: it the input array
- * @size: is the size of the array
+ * @left: starting index subarray
+ * @right: ending index subarray
  * @value: is a value to search in
  * Return: index of the number
  */
-int bn_search(int *array, size_t size, int value)
+int bin_search(int *array, size_t left, size_t right, int value)
 {
-	int index;
+	size_t i;
 
-	index = searching(array, size, value);
-
-	if (index >= 0 && array[index] != value)
+	if (array == NULL)
 		return (-1);
 
-	return (index);
+	while (right >= left)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+
+	return (-1);
 }
 
 /**
@@ -71,33 +47,18 @@ int bn_search(int *array, size_t size, int value)
  */
 int exponential_search(int *array, size_t size, int value)
 {
-	size_t index, next;
-	int result;
+	size_t i = 0, right;
 
 	if (array == NULL)
 		return (-1);
 
-	if (array[0] == value)
-		return (0);
-
-	index = 1;
-
-	while (array[index] < value && index < size)
+	if (array[0] != value)
 	{
-		printf("Value checked array[%d] = [%d]\n", (int)index, array[index]);
-		index *= 2;
+		for (i = 1; i < size && array[i] <= value; i = i * 2)
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	}
 
-	next = (index >= size) ? (size - 1) : index;
-
-	index /= 2;
-
-	printf("Value found between indexes [%d] and [%d]\n", (int)index, (int)next);
-
-	result = bn_search(array + index, (next + 1) - index, value);
-
-	if (result >= 0)
-		result += index;
-
-	return (result);
+	right = i < size ? i : size - 1;
+	printf("Value found between indexes [%ld] and [%ld]\n", i / 2, right);
+	return (bin_search(array, i / 2, right, value));
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -46,7 +46,7 @@ int searching(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
-int bn_search(int *array, size_t size, int value);
+int bin_search(int *array, size_t left, size_t right, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
