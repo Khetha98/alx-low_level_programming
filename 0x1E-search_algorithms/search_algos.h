f5ace21eba935da5a0b1906bca20f223e8d969b3
Khetha98 <Khethukuthulasimamane@gmail.com>
2024-03-16 13:52:13 +0200

    task 103 fix 1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,6 +11,7 @@ int searching(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int bn_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 
 #endif /* A094B056_D9A2_4D4B_9BC3_CC89AA69227A */
